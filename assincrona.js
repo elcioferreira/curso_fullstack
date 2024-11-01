630bff80e4119aaeb9966e89a8b337a5e1bd7f7a
elcio <eferreira@techsocial.cm.br>
2024-11-01 20:27:17 -0300

    primeiro commit

---
+++ assincrona.js
@@ -0,0 +1,8 @@
+console.log("inicio programação sincrona");
+
+// função assincrona
+setTimeout(function(){
+    console.log("|Executando função assincrona");
+    console.log("Fim função assincrona");
+},3000);
+
---
+++ nome.js
@@ -0,0 +1,14 @@
+//declaração de variaveis
+
+var nome = "Elcio";
+var sobrenome = " Ferreira"
+
+console.log("Instrução solsa em arquivo js");
+
+function exibirNome(){
+console.log("Exibir nome :"+ nome + sobrenome);
+}
+exibirNome();
+function alerta(){
+    alert("Voce apertou o botao");
+}
